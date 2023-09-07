99d296b7f0fc855c8a00a6f132cd6daecf4e828c
Mursalin Sheikh <mahigunda230@gmail.com>
2023-09-07 21:30:37 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://videoserver-lbev.onrender.com"), []);
+  const socket = useMemo(() => io('https://videoserver-lbev.onrender.com'), []);
 
   return (
     <SocketContext.Provider value={socket}>
